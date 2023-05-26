e1e5751b31749c8c82078866f100a83464a8f990
OwenSalim <125659639+OwenSalim@users.noreply.github.com>
2023-05-26 15:21:55 +0700

    merge with fauz footer

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -2,5 +2,20 @@ import Phone from "./images/Phone.svg";
 import EComplain from "./images/Ecomplainz.svg";
 import IconEcomplain from "./images/IconEcomplain.svg";
 import WhatAppIcon from "./images/CallWA.png";
+import GoogleplayIcon from "./images/gplay.png";
+import WAIcon from "./images/wa.png";
+import AppStore from "./images/appstore.png";
+import GmailIcon from "./images/gmail.png";
+import EcomplainzWhite from "./images/EcomplainzWhite.svg";
 
-export { Phone, EComplain, IconEcomplain, WhatAppIcon };
+export {
+  Phone,
+  EComplain,
+  IconEcomplain,
+  WhatAppIcon,
+  GoogleplayIcon,
+  WAIcon,
+  AppStore,
+  GmailIcon,
+  EcomplainzWhite,
+};
