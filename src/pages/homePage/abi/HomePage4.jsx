fe7775d15d422e6b9be60f61b1ee47923e74af59
AbiManyuDwiPutra <91584962+AbiManyuDwiPutra@users.noreply.github.com>
2023-06-18 21:29:21 +0700

    update homepage

--- src/pages/homePage/abi/HomePage4.jsx
+++ src/pages/homePage/abi/HomePage4.jsx
@@ -1,54 +1,40 @@
 import React from "react";
 import { Row, Col, Typography } from "antd";
 import "./homePage4.css";
-import Gap from "../../../components/gap/Gap";
-
-const { Title, Paragraph } = Typography;
+import { Gembok, Pesan } from "../../../assets";
 
 const HomePage4 = () => {
   return (
-    <div className="homePage">
-      <Row justify="end" className="section">
-        <Col xs={24} sm={24} md={18} lg={12}>
-          <div className="content-1">
-            <Title level={2} style={{ fontWeight: "bold" }}>
-              Apakah identitas pelapor aman?
-            </Title>
-            <Paragraph>
-              Identitas Pelapor dijaga keamanannya, anda bisa mengeluarkan keluh
-              kesah tanpa rasa <br /> khawatir. Anda bisa memilih apakah
-              identitas andaakan dirahasiakan atau <br /> dipublikasikan.
-            </Paragraph>
-          </div>
+    <div className="privasi">
+      <h1 className="title-privasi">
+        Privasi kalian adalah prioritas COMPLAINZ
+      </h1>
+      <Row justify="center" className="contentMaster">
+        <Col>
+          <Row align="middle">
+            <Col>
+              <img height={168} src={Gembok} alt="gembok" />
+            </Col>
+            <Col className="content-privasi">
+              <h1 className="titleContentPriv">Tetap Anonim</h1>
+              <h2 className="paragrafContentPriv">
+                Tidak ada yang mengetahui data kalian
+              </h2>
+            </Col>
+          </Row>
         </Col>
-      </Row>
-      <Gap height={50} />
-      <Row justify="left" className="section">
-        <Col xs={24} sm={24} md={18} lg={12}>
-          <div className="content-2">
-            <Title level={2} style={{ fontWeight: "bold" }}>
-              Kenapa Laporan ditolak?
-            </Title>
-            <Paragraph>
-              Anda mungkin menyalahi salah satu aturan untuk menyampaikan
-              laporan. Salah satunya Berkata kasar, Menyampaikan berita hoax,
-              atau permasalahan yang fiktif.
-            </Paragraph>
-          </div>
-        </Col>
-      </Row>
-      <Gap height={50} />
-      <Row justify="end" className="section">
-        <Col xs={24} sm={24} md={18} lg={12}>
-          <div className="content-1">
-            <Title level={2} style={{ fontWeight: "bold" }}>
-              Bagaimana cara mengetahui Proses laporan?
-            </Title>
-            <Paragraph>
-              Pada aplikasi Complainz, anda dapat melihat secara detail proses
-              dan progres laporan <br /> anda, tanpa perlu merasa bingung.
-            </Paragraph>
-          </div>
+        <Col>
+          <Row align="middle">
+            <Col>
+              <img height={168} src={Pesan} alt="gembok" />
+            </Col>
+            <Col className="content-privasi">
+              <h1 className="titleContentPriv">Tidak Berbagi </h1>
+              <h2 className="paragrafContentPriv">
+                Laporan tidak bisa di share jika bersifat privat/rahasia
+              </h2>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </div>
