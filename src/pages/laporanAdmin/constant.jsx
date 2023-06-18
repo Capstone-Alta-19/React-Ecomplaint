56edecdb994a8685e6f2efa27e00b1f4f255f07d
AbiManyuDwiPutra <91584962+AbiManyuDwiPutra@users.noreply.github.com>
2023-06-18 21:51:10 +0700

    dashboard laporan

---
+++ src/pages/laporanAdmin/constant.jsx
@@ -0,0 +1,26 @@
+export const data_table = [
+  {
+    key: "1",
+    type: "Complaint",
+    categories: "Sarana dan Prasarana",
+    tanggal: "01/02/2023",
+    isi: "Komputer",
+    status: "Terjawab"
+  },
+  {
+    key: "2",
+    type: "Complaint",
+    categories: "Sarana...",
+    tanggal: "01/02/2023",
+    isi: "Wifi Lemot",
+    status: "Terjawab"
+  },
+  {
+    key: "2",
+    type: "Complaint",
+    categories: "Sarana...",
+    tanggal: "01/02/2023",
+    isi: "Wifi Lemot",
+    status: "Terjawab"
+  },
+];
