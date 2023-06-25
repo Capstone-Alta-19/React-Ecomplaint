027a850d891fe2f64b820577bc60131de8d89b4d
AbiManyuDwiPutra <91584962+AbiManyuDwiPutra@users.noreply.github.com>
2023-06-26 01:25:54 +0700

    update dashboard admin fauz

--- src/pages/dashboardAdmin/DashboardAdmin.jsx
+++ src/pages/dashboardAdmin/DashboardAdmin.jsx
@@ -1,8 +1,212 @@
 import React from "react";
+import { Row, Col, Card, Button, Space } from "antd";
+import { useState } from "react";
+import Gap from "../../components/gap/Gap";
 import "./dashboardAdmin.css";
+import { useGetDashboard } from "./hooks/useDashboard";
+import { Link } from "react-router-dom";
 
 const DashboardAdmin = () => {
-  return <div>Dashboard</div>;
+  const [size, setSize] = useState("small");
+  const [sort, setSort] = useState("asc");
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState("1");
+  const [limit, setLimit] = useState("50");
+  const [type, setType] = useState("All");
+
+  const [isLoadingData, dashboardData, getDashboardData] = useGetDashboard();
+
+  const handleSetType = React.useCallback((newType) => {
+    setType(newType);
+  }, []);
+
+  React.useEffect(() => {
+    getDashboardData(sort, type, search, page, limit);
+  }, [sort, type, search, page, limit]);
+
+  return (
+    <div className="content-dashboard">
+      {/* heading */}
+      <Row>
+        <Col span={12}>
+          <div>
+            <Button className="button-view2">
+              <Link
+                to={`/dashboard-overview/All`}
+                onClick={() => handleSetType("All")}
+              >
+                View to edit
+              </Link>
+            </Button>
+          </div>
+        </Col>
+        <Col span={12}>
+          <div>
+            <Button className="button-view2">
+              <Link
+                to={`/dashboard-overview/Complaint`}
+                onClick={() => handleSetType("Complaint")}
+              >
+                View to edit
+              </Link>
+            </Button>
+          </div>
+        </Col>
+      </Row>
+
+      {/* kotak konten */}
+      <Row>
+        {/* KOLOM 1 */}
+        <Col span={12}>
+          <Card
+            className="card-shadow"
+            style={{
+              width: 420,
+            }}
+          >
+            <div className="recently-added">Recently Added</div>
+            <Gap height={20} />
+
+            {isLoadingData ? (
+              <p>Loading data...</p>
+            ) : (
+              dashboardData?.complaints?.slice(0, 10).map((item) => (
+                <div key={item.id}>
+                  <div id="judul-komplain">
+                    <b>{item.name}</b>
+                    <Space wrap>
+                      <Button
+                        className="status1"
+                        size={size}
+                        type="primary"
+                        shape="round"
+                      >
+                        <span className="status-text">{item.type}</span>
+                      </Button>
+                      <Button className="status2" size={size} shape="round">
+                        <span className="status-text">{item.status}</span>
+                      </Button>
+                    </Space>
+                    <div className="isi-komplain">{item.description}</div>
+                  </div>
+                  <Gap height={5} />
+                </div>
+              ))
+            )}
+          </Card>
+        </Col>
+
+        {/* KOLOM 2 */}
+        <Col span={12}>
+          {/* kotak complain */}
+          {/* <Button className="button-view2">View to edit</Button> */}
+          <Row>
+            <Col>
+              <Card
+                className="card-shadow"
+                style={{
+                  width: 443,
+                  height: 291,
+                }}
+              >
+                {isLoadingData ? (
+                  <p>Loading data...</p>
+                ) : (
+                  dashboardData?.complaints
+                    ?.filter((item) => item.type === "Complaint")
+                    .slice(0, 5)
+                    .map((item) => (
+                      <div key={item.id}>
+                        <div id="judul-komplain">
+                          <b>{item.name}</b>
+                          <Space wrap>
+                            <Button
+                              className="status1"
+                              size={size}
+                              type="primary"
+                              shape="round"
+                            >
+                              <span className="status-text">{item.type}</span>
+                            </Button>
+                            <Button
+                              className="status2"
+                              size={size}
+                              shape="round"
+                            >
+                              <span className="status-text">{item.status}</span>
+                            </Button>
+                          </Space>
+                          <div className="isi-komplain">{item.description}</div>
+                        </div>
+                        <Gap height={5} />
+                      </div>
+                    ))
+                )}
+              </Card>
+            </Col>
+          </Row>
+          <Gap height={40} />
+
+          {/* kotak aspirasi */}
+          <Button className="button-view3">
+            <Link
+              to={`/dashboard-overview/Aspiration`}
+              onClick={() => handleSetType("Aspiration")}
+            >
+              View to edit
+            </Link>
+          </Button>
+          <Row>
+            <Col>
+              <Card
+                className="card-shadow"
+                hoverable
+                style={{
+                  width: 443,
+                  height: 291,
+                  color: "#19345E",
+                }}
+              >
+                {isLoadingData ? (
+                  <p>Loading data...</p>
+                ) : (
+                  dashboardData?.complaints
+                    ?.filter((item) => item.type === "Aspiration")
+                    .slice(0, 4)
+                    .map((item) => (
+                      <div key={item.id}>
+                        <div id="judul-komplain">
+                          <b>{item.name}</b>
+                          <Space wrap>
+                            <Button
+                              className="status1-aspirasi"
+                              size={size}
+                              type="primary"
+                              shape="round"
+                            >
+                              <span className="status-text">{item.type}</span>
+                            </Button>
+                            <Button
+                              className="status2"
+                              size={size}
+                              shape="round"
+                            >
+                              <span className="status-text">{item.status}</span>
+                            </Button>
+                          </Space>
+                          <div className="isi-komplain">{item.description}</div>
+                        </div>
+                        <Gap height={5} />
+                      </div>
+                    ))
+                )}
+              </Card>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default DashboardAdmin;
--- src/pages/detailComplaint/hooks/useDashboard.jsx
+++ src/pages/detailComplaint/hooks/useDashboard.jsx
@@ -2,10 +2,6 @@ import { useCallback, useState } from "react";
 import { api } from "../../../api";
 import { message } from "antd";
 
- 
-
-
-
 export const useGetDashboard = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState();
@@ -40,100 +36,84 @@ export const useGetDashboard = () => {
 };
 
 export const useUpdateDashboard = () => {
-  const [isLoading,setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const updateDashboardData = useCallback(async (id, body, onSuccess) => {
-      try{
-          console.log(id)
-          setIsLoading(true);
-          await api.dashboardUpdate(id, body)
-         onSuccess && onSuccess();
-      } catch (err) {
-          message.open({
-              type: 'error',
-              content:   `${err?.message}`,
-            });
-      } finally {
-          setIsLoading(false);
-          message.open({
-              type: 'success',
-              content: 'berhasil update',
-             
-            })
-            console.log(id);
+    try {
+      console.log(id);
+      setIsLoading(true);
+      await api.dashboardUpdate(id, body);
+      onSuccess && onSuccess();
+    } catch (err) {
+      message.open({
+        type: "error",
+        content: `${err?.message}`,
+      });
+    } finally {
+      setIsLoading(false);
+      message.open({
+        type: "success",
+        content: "berhasil update",
+      });
+      console.log(id);
+    }
+  }, []);
 
-      }
-      },[])
-
-      
-
-  
-
-      return [isLoading, updateDashboardData]
+  return [isLoading, updateDashboardData];
 };
 
 export const useDeleteDashboard = () => {
-  const [isLoading,setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const deleteDashboardData = useCallback(async (id, onSuccess) => {
-      try{
-          setIsLoading(true);
-          await api.dashboardDelete(id)
-         onSuccess && onSuccess();
-      } catch (err) {
-          message.open({
-              type: 'error',
-              content:   `${err?.message}`,
-            });
-      } finally {
-          setIsLoading(false);
-          message.open({
-              type: 'success',
-              content: 'berhasil hilang',
-            });
+    try {
+      setIsLoading(true);
+      await api.dashboardDelete(id);
+      onSuccess && onSuccess();
+    } catch (err) {
+      message.open({
+        type: "error",
+        content: `${err?.message}`,
+      });
+    } finally {
+      setIsLoading(false);
+      message.open({
+        type: "success",
+        content: "berhasil hilang",
+      });
+    }
+  }, []);
 
-      }
-      },[])
-
-      
-
-  
-
-      return [isLoading, deleteDashboardData]
+  return [isLoading, deleteDashboardData];
 };
-
 
 export const useGetCSV = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState();
 
-  const getCSV = useCallback(
-    async (sort, type, search, limit, onSuccess) => {
-      try {
-        setIsLoading(true);
-        const res = await api.getCSVData(sort, type, search, limit);
-        if (res) {
-          setData(res.data);
-          
-          //onSuccess && onSuccess();
-          message.open({
-            type: "success",
-            content: " Data berhasil dimuat",
-          });
-          setIsLoading(false);
-        }
-      } catch (err) {
+  const getCSV = useCallback(async (sort, type, search, limit, onSuccess) => {
+    try {
+      setIsLoading(true);
+      const res = await api.getCSVData(sort, type, search, limit);
+      if (res) {
+        setData(res.data);
+
+        //onSuccess && onSuccess();
         message.open({
-          type: "error",
-          content: `${err?.message}`,
+          type: "success",
+          content: " Data berhasil dimuat",
         });
-      } finally {
         setIsLoading(false);
       }
-    },
-    []
-  );
+    } catch (err) {
+      message.open({
+        type: "error",
+        content: `${err?.message}`,
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
 
   return [isLoading, data, getCSV];
 };
-
