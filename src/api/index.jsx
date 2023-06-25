a678bf1409cffadfc0418501902b0c56511bde32
AbiManyuDwiPutra <91584962+AbiManyuDwiPutra@users.noreply.github.com>
2023-06-25 17:53:19 +0700

    add tambah berita

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -4,34 +4,23 @@ export const api = {
   //API
   //Admin Login
   login: (body) => {
-    return baseAPI
-      .post("/login/admin", body)
-
+    return baseAPI.post("/login/admin", body);
   },
   dashboard: (sort, type, search, page, limit) => {
     return baseAPI.get(
       `/dashboard/complaint?sort=${sort}&type=${type}&search=${search}&page=${page}&limit=${limit}`
-    )
-    ;
-    
+    );
   },
   dashboardUpdate: (id, body) => {
-    return baseAPI.put(
-      `/dashboard/complaint/${id}`, body
-    )
-    ;
+    return baseAPI.put(`/dashboard/complaint/${id}`, body);
   },
   dashboardDelete: (id) => {
-    return baseAPI.delete(
-      `/dashboard/complaint/${id}`
-    )
-    ;
+    return baseAPI.delete(`/dashboard/complaint/${id}`);
   },
   getCSVData: (sort, type, search, limit) => {
     return baseAPI.get(
       `/dashboard/complaint/export?sort=${sort}&type=${type}&search=${search}&limit=${limit}`
-    )
-    ;
+    );
   },
 
   dashboard: (sort, type, search, page, limit) => {
@@ -52,9 +41,9 @@ export const api = {
     return baseAPI.post(`/dashboard/complaint/${id}`, body);
   },
   newsGet: () => {
-    return baseAPI.get(
-      `/news`
-    )
-    ;
+    return baseAPI.get(`/news`);
+  },
+  postBerita: (body) => {
+    return baseAPI.post(`/dashboard/news`,body)
   }
 };
--- src/pages/beritaAdmin/tambahBerita/TambahBerita.jsx
+++ src/pages/beritaAdmin/tambahBerita/TambahBerita.jsx
@@ -1,11 +1,105 @@
-import React from 'react';
+import { Button, Form, Input, Radio, Select } from "antd";
+import { React, useState } from "react";
+import { useGetBerita, usePostBerita } from "../hooks/useBerita";
+import "./tambahBerita.css";
+
+// const [isloadingAddBerita, AddBerita, postAddBerita] = usePostBerita();
+// const [isLoadingBerita, dataBerita, getBeritaData] = useGetBerita();
+
+// const postBerita = (body) => {
+//   postAddBerita(body, () => {
+//     getBeritaData();
+//   });
+// };
 
 const TambahBerita = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  const { TextArea } = Input;
+  return (
+    <div className="add-berita">
+      <h1 className="titlePage-berita">Add Berita</h1>
+      <Form
+        // className="formDetailData"
+        // onFinish={postBerita}
+        layout="horizontal"
+        labelAlign="left"
+        labelCol={{
+          span: 6,
+        }}
+        colon={false}
+        fields={[
+          {
+            name: "status",
+            // value: rowData?.status,
+          },
+          {
+            name: "type",
+            // value: rowData?.type,
+          },
+        ]}
+      >
+        <Form.Item
+          name="news_name"
+          label="Judul Berita"
+          rules={[
+            {
+              message: "Please Input Your Name!",
+            },
+          ]}
+        >
+          <Input placeholder="Masukkan Nama" />
+        </Form.Item>
+        <Form.Item label="Status" name="status">
+          {/* <Select
+            placeholder="Stats"
+            options={[
+              {
+                value: "Pending",
+                label: "Pending",
+              },
+              {
+                value: "Proccess",
+                label: "Proccess",
+              },
+              {
+                value: "Resolved",
+                label: "Resolved",
+              },
+            ]}
+          ></Select> */}
+          <Radio.Group size="large">
+            <Radio.Button value="a">Akademik</Radio.Button>
+            <Radio.Button value="b">Ormas</Radio.Button>
+            <Radio.Button value="c">Mahasiswa</Radio.Button>
+            <Radio.Button value="d">Dosen</Radio.Button>
+            <Radio.Button value="e">Lainnya</Radio.Button>
+            <Radio.Button value="f">Sarana dan Prasarana</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item
+          label="Isi berita"
+          name="description"
+          rules={[
+            {
+              // required: true,
+              message: "Please Input Your Name!",
+            },
+          ]}
+        >
+          <TextArea
+            // className="inputFeedback"
+            placeholder="Isi berita..."
+            autoSize={{
+              minRows: 6,
+              maxRows: 10,
+            }}
+          />
+        </Form.Item>
+        <Button type="primary" htmlType="submit" className="btnAddBerita">
+          Kirim
+        </Button>
+      </Form>
+    </div>
+  );
+};
 
 export default TambahBerita;
